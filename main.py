af1250b34a7d1008b35c322f76367ccea3f2fa2f
ergynplavci <ergyn.plavci@hotmail.com>
2025-09-16 18:29:28 +0200

    The game now opens a window that user can close

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    print("""Starting Asteroids!
+            Screen width: 1280
+            Screen height: 720""")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-
-    print("""Starting Asteroids!
-            Screen width: 1280
-            Screen height: 720""")
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
